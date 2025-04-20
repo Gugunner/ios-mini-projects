0e0a2d43efc8482cb60d135a8ef687fd7af446ab
Raul Alonzo Mendoza <ram2489@gmail.com>
2025-04-20 14:56:10 -0500

    Add implementation of app service context for mocked data

---
+++ feed_table_view_sample/feed_table_view_sample/Context/MockAppServiceContext.swift
@@ -0,0 +1,10 @@
+//
+//  MockAppServiceContext.swift
+//  feed_table_view_sample
+//
+//  Created by Raul_Alonzo on 20/04/25.
+//
+
+class MockAppServiceContext: AppServiceContext {
+    var feedLoader: FeedLoaderProtocol = FeedLoader()
+}
