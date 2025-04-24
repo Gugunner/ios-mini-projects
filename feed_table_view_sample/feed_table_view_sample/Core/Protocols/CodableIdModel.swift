60f581bd7adf18bfe2fd138877c9081992de2883
Raul Alonzo <ram2489@gmail.com>
2025-04-24 15:30:34 -0500

    Create protocol that enforces how to identify a model for encoding
    This protocol is to make sure all Data model conforms to being Codable and have a unique identifier.

---
+++ feed_table_view_sample/feed_table_view_sample/Core/Protocols/CodableIdModel.swift
@@ -0,0 +1,13 @@
+//
+//  AppIdentifiable.swift
+//  feed_table_view_sample
+//
+//  Created by Raul_Alonzo on 24/04/25.
+//
+
+import Foundation
+
+protocol CodableIdModel: Codable {
+    var identifier: UUID { get }
+    static var modelKey: DataModelCodingKey {get}
+}
