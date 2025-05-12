7385d9383f8e66381066730aa5f1892c4f1c6cf6
Raul Alonzo Mendoza <ram2489@gmail.com>
2025-05-12 13:36:20 -0500

    Add model for the message

---
+++ stream_messages_sample/stream_messages_sample/Data/Model/Message.swift
@@ -0,0 +1,12 @@
+//
+//  Message.swift
+//  stream_messages_sample
+//
+//  Created by Raul_Alonzo on 12/05/25.
+//
+
+struct Message {
+    let id: Int
+    var primaryText: String
+    var secondaryText: String
+}
