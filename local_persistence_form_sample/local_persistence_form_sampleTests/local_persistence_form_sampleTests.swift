03d4ed83d77874f7b8cd0c478ab493d7c8553331
Raul Alonzo Mendoza <ram2489@gmail.com>
2025-05-13 10:29:03 -0500

    Add base for local_persistence_form_sample project

---
+++ local_persistence_form_sample/local_persistence_form_sampleTests/local_persistence_form_sampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  local_persistence_form_sampleTests.swift
+//  local_persistence_form_sampleTests
+//
+//  Created by Raul_Alonzo on 13/05/25.
+//
+
+import XCTest
+@testable import local_persistence_form_sample
+
+final class local_persistence_form_sampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
